7fc30461f3f44aa96268a07bdcc5e1dae5a4f47d
lmatheson2026 <144724348+lmatheson2026@users.noreply.github.com>
2023-10-16 20:07:51 -0400

    Add files via upload

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,33 +1,29 @@
-
 public class BinaryTree {
 	protected node root;
-public class node {
-	int data;
-	node left;
-	node right; 
-	public node(int data) {
-		this.data = data;
-		this.left = null;
-		this.right = null;
-	}
-		
-}
-public void testing() {
-	
-}
+	public class node {
+		int data;
+		node left;
+		node right; 
+		public node(int data) {
 
-public static void main(String[] args) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+
+	}
+	public void testing() {
+		
 	
-	
-	 BTPrinting Z = new  BTPrinting();
-	 searchBT Y = new  searchBT();
-	aggregateBT X = new aggregateBT();
-	Z.postOrder();
-	Z.printRight();
-	Y.containsGreater();
-	Y.getLevel();
-	X.countEvenNumber();
-	X.findSum();
-}
+	}
+
+	public static void main(String[] args) {
+		aggregateBT X = new aggregateBT();
+		BTPrinting Y = new BTPrinting();
+		 searchBT Z = new  searchBT();
+		 Z.testing();
+		Y.testing();
+		X.testing();
+	}
 
 }
--- aggregateBT.java
+++ aggregateBT.java
@@ -1,4 +1,51 @@
 
-public class aggregateBT {
+public class aggregateBT extends BinaryTree {
+@Override
+	public void testing() {
+    root = new node(5);
+    root.left = new node(3);
+    root.left.right = new node(4);
+    root.right = new node(7);
+    root.right.left = new node(6);
+    root.right.right = new node(8);
+    System.out.println(findSum());
+	System.out.println(countEvenNumber());
 
+	}
+
+	public int findSum() {
+return findSumHelper(root);
+	}
+
+		public int findSumHelper(node temp) {
+		    if (temp == null)
+		        return 0;
+
+		    int leftSum = findSumHelper(temp.left);
+		    int rightSum = findSumHelper(temp.right);
+
+		    return leftSum + rightSum + temp.data;
+		
+	}
+
+
+	public int countEvenNumber() {
+return countEvenNumberHelper(root);
+	}
+
+	public int countEvenNumberHelper(node temp) {
+		 int currentNodeCount = 0;
+		 if (temp == null)
+		        return 0;
+
+		    int t1 = countEvenNumberHelper(temp.left);
+		    int t2 = countEvenNumberHelper(temp.right);
+
+		    // Check if the current node's data is even
+		    if(temp.data % 2 == 0)
+		     currentNodeCount += 1;
+
+		    return t1 + t2 + currentNodeCount;
+	}
 }
+
